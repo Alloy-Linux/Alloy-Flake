9fd7c919c379d2f6f7940c672cbb671eca084b74
Simon-Weij <simongvanderweij@gmail.com>
2025-08-28 16:05:03 +0200

    iowjfow

--- modules/secureboot.nix
+++ modules/secureboot.nix
@@ -7,20 +7,7 @@
   
   system.activationScripts.secureboot = {
     text = ''
-      if ${pkgs.sbctl}/bin/sbctl status | grep -qE "Secure Boot:.*Enabled"; then
-        echo "Secure boot is already enrolled"
-        exit 0
-      fi
-
-      # Create secure boot keys and enroll to uefi
-      ${pkgs.sbctl}/bin/sbctl create-keys
-
-      if ${pkgs.sbctl}/bin/sbctl enroll-keys; then
-        echo "Secure boot keys has been enrolled"
-      else
-        ${pkgs.sbctl}/bin/sbctl status
-      fi
-        '';
+    '';
   };
 
   boot.bootspec.enable = true;
